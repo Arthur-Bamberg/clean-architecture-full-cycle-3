bf6e55dc61e1c03462be53fb2a02452da2260e47
Arthur-Bamberg <bamberguisses@gmail.com>
2025-05-18 10:21:16 -0300

    feat: implement find product use case and corresponding DTOs

---
+++ src/usecase/product/find/find.product.integration.spec.ts
@@ -0,0 +1,70 @@
+import { Sequelize } from "sequelize-typescript";
+import ProductModel from "../../../infrastructure/product/repository/sequelize/product.model";
+import ProductRepository from "../../../infrastructure/product/repository/sequelize/product.repository";
+import Product from "../../../domain/product/entity/product";
+import { FindProductUseCase } from "./find.product.usecase";
+
+describe("Test find customer use case", () => {
+  let sequelize: Sequelize;
+
+  beforeEach(async () => {
+    sequelize = new Sequelize({
+      dialect: "sqlite",
+      storage: ":memory:",
+      logging: false,
+      sync: { force: true },
+    });
+
+    sequelize.addModels([ProductModel]);
+    await sequelize.sync();
+  });
+
+  afterEach(async () => {
+    await sequelize.close();
+  });
+
+  it("should find a product", async () => {
+    const productRepository = new ProductRepository();
+    const usecase = new FindProductUseCase(productRepository);
+
+    const product = new Product("123", "Product 1", 100);
+
+    await productRepository.create(product);
+
+    const input = {
+      id: "123",
+    };
+
+    const output = {
+      id: "123",
+      name: "Product 1",
+      price: 100,
+    };
+
+    const result = await usecase.execute(input);
+
+    expect(result).toEqual(output);
+  });
+
+  it("should not find a product", async () => {
+    const MockRepository = {
+      find: jest.fn(),
+      findAll: jest.fn(),
+      create: jest.fn(),
+      update: jest.fn(),
+    };
+    MockRepository.find.mockImplementation(() => {
+      throw new Error("Product not found");
+    });
+    const usecase = new FindProductUseCase(MockRepository);
+
+    const input = {
+      id: "123",
+    };
+
+    expect(() => {
+      return usecase.execute(input);
+    }).rejects.toThrow("Product not found");
+  });
+});
+
