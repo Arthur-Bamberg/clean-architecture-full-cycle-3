066c81d41c7fc6251520bec2de66c05fbea2fd7d
Arthur-Bamberg <bamberguisses@gmail.com>
2025-05-18 10:54:16 -0300

    feat: add integration tests for create product use case with validation checks

---
+++ src/usecase/product/create/create.product.integration.spec.ts
@@ -0,0 +1,69 @@
+import { Sequelize } from "sequelize-typescript";
+import ProductModel from "../../../infrastructure/product/repository/sequelize/product.model";
+import ProductRepository from "../../../infrastructure/product/repository/sequelize/product.repository";
+import { CreateProductUseCase } from "./create.product.usecase";
+
+const input = {
+  name: "Product",
+  price: 100,
+};
+
+describe("Integration test create product use case", () => {
+  let sequelize: Sequelize;
+
+  beforeEach(async () => {
+    sequelize = new Sequelize({
+      dialect: "sqlite",
+      storage: ":memory:",
+      logging: false,
+      sync: { force: true },
+    });
+
+    sequelize.addModels([ProductModel]);
+    await sequelize.sync();
+
+    input.name = "Product";
+    input.price = 100;
+  });
+
+  afterEach(async () => {
+    await sequelize.close();
+  });
+
+  it("should create a product", async () => {
+    const productRepository = new ProductRepository();
+    const productCreateUseCase = new CreateProductUseCase(productRepository);
+
+    const output = await productCreateUseCase.execute(input);
+
+    expect(output).toEqual({
+      id: expect.any(String),
+      name: input.name,
+      price: input.price,
+    });
+  });
+
+  it("should thrown an error when name is missing", async () => {
+    const customerRepository = new ProductRepository();
+    const customerCreateUseCase = new CreateProductUseCase(customerRepository);
+
+    input.name = "";
+
+    await expect(customerCreateUseCase.execute(input)).rejects.toThrow(
+      "Name is required"
+    );
+  });
+
+  it("should thrown an error when price is lower than zero", async () => {
+    const customerRepository = new ProductRepository();
+    const customerCreateUseCase = new CreateProductUseCase(customerRepository);
+
+    input.price = -1;
+
+    await expect(customerCreateUseCase.execute(input)).rejects.toThrow(
+      "Price must be greater than zero"
+    );
+  });
+});
+
+
